f897619cdb39c0a146d53bb8e0cb66a6b7babd36
NelsonRay <nelsonray27@gmail.com>
2024-01-30 16:35:10 -0600

    Added skeleton and app shell WIP

---
+++ src/lib/components/AppSidebar/AppSidebar.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
+	import Contracts from '$lib/icons/Contracts.svg';
+	import Workflows from '$lib/icons/Workflows.svg';
+	import Reports from '$lib/icons/Reports.svg';
+
+	let currentTile = 0;
+</script>
+
+<AppRail>
+	<!-- <svelte:fragment slot="lead">
+		<AppRailAnchor href="/">(icon)</AppRailAnchor>
+	</svelte:fragment> -->
+	<!-- --- -->
+	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+		<svelte:fragment slot="lead"
+			><div class="flex items-center justify-center">
+				<img src={Contracts} class="h-12 w-12" />
+			</div>
+		</svelte:fragment>
+		<span>Contracts</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+		<svelte:fragment slot="lead"
+			><div class="flex items-center justify-center">
+				<img src={Workflows} class="h-12 w-12" />
+			</div></svelte:fragment
+		>
+		<span>Workflows</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+		<svelte:fragment slot="lead"
+			><div class="flex items-center justify-center">
+				<img src={Reports} class="h-10 w-10" />
+			</div></svelte:fragment
+		>
+		<span>Reports</span>
+	</AppRailTile>
+	<!-- --- -->
+	<!-- <svelte:fragment slot="trail">
+		<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+	</svelte:fragment> -->
+</AppRail>
---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import AppBar from '$lib/components/AppBar/AppBar.svelte';
+	import AppSidebar from '$lib/components/AppSidebar/AppSidebar.svelte';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header"><AppBar /></svelte:fragment>
+	<svelte:fragment slot="sidebarLeft"><AppSidebar /></svelte:fragment>
+	<slot />
+</AppShell>
