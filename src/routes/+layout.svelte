23f78ef2a65f9b353681c1ab6c764f4099ecb44d
NelsonRay <nelsonray27@gmail.com>
2024-02-01 20:16:38 -0600

    Retry OneSignal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,40 @@
 <script>
+	// @ts-nocheck
+
 	import '../app.postcss';
+
+	// Import the onMount function from Svelte
+	import { onMount } from 'svelte';
+
+	// Function to load OneSignal script
+	function loadOneSignalScript() {
+		const script = document.createElement('script');
+		script.src = 'https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js';
+		script.defer = true;
+
+		// Append the script to the document head
+		document.head.appendChild(script);
+
+		// Initialize OneSignal when the script is loaded
+		script.onload = () => {
+			window.OneSignalDeferred = window.OneSignalDeferred || [];
+			window.OneSignalDeferred.push(function (OneSignal) {
+				OneSignal.init({
+					appId: '58a83d45-05f1-438a-878e-9e5830a4e90d',
+					safari_web_id: 'web.onesignal.auto.5ccade99-0f35-4775-9ae0-5e2c3bfd110b',
+					notifyButton: {
+						enable: true
+					}
+				});
+			});
+		};
+	}
+
+	// Call the function when the component is mounted
+	onMount(() => {
+		loadOneSignalScript();
+		Notification.requestPermission();
+	});
 </script>
 
 <slot />
