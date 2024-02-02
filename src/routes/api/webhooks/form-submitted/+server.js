59ded0a368c8e7d6f8579d3834d6bcb56adbba61
NelsonRay <nelsonray27@gmail.com>
2024-02-02 09:10:46 -0600

    webhook initial test

--- src/routes/api/webhooks/form-submitted/+server.js
+++ src/routes/api/webhooks/form-submitted/+server.js
@@ -1,8 +1,66 @@
-import { json } from '@sveltejs/kit';
+// @ts-nocheck
 
-export async function POST({ request }) {
-	const payload = await request.json();
-	console.log(payload);
+import { SUPABASE_URL, SUPABASE_ANON_KEY, SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
+import { json } from '@sveltejs/kit';
+import { createServerClient } from '@supabase/ssr';
+
+// 50e95568-180b-46d5-a341-f216bb2a3c17 Opp
+// 0ce91293-4277-446e-9ff2-2d5c62bbb0fe Eng
+// bee07e8a-3c83-4bce-89a7-f91ca65804e6 Bom
+// 18055704-d9b9-42d7-958b-f5d1d5b1ba4d Pur
+// 53cc6979-4406-47aa-97a0-1d83d0504c12 Lab
+// 6bbf4342-1b50-4c1a-9dc5-ad40562c5626 Rev
+// 6a0d1585-d572-4d8f-bdb4-498a89506e85 Bid
+
+export async function POST({ request, cookies }) {
+	const {
+		record: { form, response, solicitation_matched }
+	} = await request.json();
+
+	/** @type {import('@supabase/supabase-js').SupabaseClient<import('$lib/server/types/supabase.js').Database>} */
+	const supabase = createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+		// @ts-ignore
+		cookies,
+		global: {
+			headers: { Authorization: `Bearer ${SUPABASE_SERVICE_ROLE_KEY}` }
+		}
+	});
+
+	const { data, error } = await supabase
+		.from('solicitations_matched')
+		.update(response)
+		.eq('id', solicitation_matched)
+		.select('*');
+
+	if (error) {
+		console.log(error);
+	}
+
+	const solicitation_matched_data = data[0];
+
+	switch (form) {
+		case '50e95568-180b-46d5-a341-f216bb2a3c17':
+			if (solicitation_matched_data.opportunity_status === '70e84e79-dea9-4269-ad49-f6952caf9a3d') {
+				await supabase
+					.from('forms')
+					.insert({ form: '0ce91293-4277-446e-9ff2-2d5c62bbb0fe', solicitation_matched });
+			}
+			break;
+		case '0ce91293-4277-446e-9ff2-2d5c62bbb0fe':
+			break;
+		case 'bee07e8a-3c83-4bce-89a7-f91ca65804e6':
+			break;
+		case '18055704-d9b9-42d7-958b-f5d1d5b1ba4d':
+			break;
+		case '53cc6979-4406-47aa-97a0-1d83d0504c12':
+			break;
+		case '6bbf4342-1b50-4c1a-9dc5-ad40562c5626':
+			break;
+		case '6a0d1585-d572-4d8f-bdb4-498a89506e85':
+			break;
+		default:
+			break;
+	}
 
 	return json({}, { status: 200 });
 }
