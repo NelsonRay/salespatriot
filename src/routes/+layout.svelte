1e0ab77a131a963049da6742058222a3c29d40af
NelsonRay <nelsonray27@gmail.com>
2024-02-01 19:40:45 -0600

    Fix to FCM

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.postcss';
 	import { initializeApp } from 'firebase/app';
 	import { getMessaging, getToken } from 'firebase/messaging';
+	import { onMount } from 'svelte';
 
 	const firebaseConfig = {
 		apiKey: 'AIzaSyC6zbaT6KMq2qpiA2X0JBKLVq7eLCbP2Dc',
@@ -12,33 +13,35 @@
 		appId: '1:882639995466:web:1645f546b86fe496325cb3'
 	};
 
-	// Initialize Firebase
-	const app = initializeApp(firebaseConfig);
-	const messaging = getMessaging(app);
-	getToken(messaging, {
-		vapidKey:
-			'BAk_1AoMZvNHprzPdI9rHs6cFrx82cvn9E5lgEmpOw8eTcfbYl-HnqOLDqUk2A7H9XBOPhY56NDR6td-MRBYbGA'
-	})
-		.then((currentToken) => {
-			if (currentToken) {
-				console.log('t:', currentToken);
-				// Send the token to your server and update the UI if necessary
-				// ...
-			} else {
-				// Show permission request UI
-				console.log('No registration token available. Request permission to generate one.');
-				// ...
-			}
+	onMount(() => {
+		// Initialize Firebase
+		const app = initializeApp(firebaseConfig);
+		const messaging = getMessaging(app);
+		getToken(messaging, {
+			vapidKey:
+				'BAk_1AoMZvNHprzPdI9rHs6cFrx82cvn9E5lgEmpOw8eTcfbYl-HnqOLDqUk2A7H9XBOPhY56NDR6td-MRBYbGA'
 		})
-		.catch((err) => {
-			console.log('An error occurred while retrieving token. ', err);
-			// ...
-		});
+			.then((currentToken) => {
+				if (currentToken) {
+					console.log('t:', currentToken);
+					// Send the token to your server and update the UI if necessary
+					// ...
+				} else {
+					// Show permission request UI
+					console.log('No registration token available. Request permission to generate one.');
+					// ...
+				}
+			})
+			.catch((err) => {
+				console.log('An error occurred while retrieving token. ', err);
+				// ...
+			});
 
-	Notification.requestPermission().then((permission) => {
-		if (permission === 'granted') {
-			console.log('Notification permission granted.');
-		}
+		Notification.requestPermission().then((permission) => {
+			if (permission === 'granted') {
+				console.log('Notification permission granted.');
+			}
+		});
 	});
 </script>
 
