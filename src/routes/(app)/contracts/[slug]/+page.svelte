b3785f0f771641f686cb5f50aad68d4a0591d4b0
NelsonRay <nelsonray27@gmail.com>
2024-02-15 13:55:02 -0500

    Table component

--- src/routes/(app)/contracts/[slug]/+page.svelte
+++ src/routes/(app)/contracts/[slug]/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	// @ts-nocheck
-	import { tableFieldMapper } from '$lib/mappers';
 	import { onMount } from 'svelte';
 	import { tags } from '$lib/tags.js';
 	import ContractViews from '$lib/components/app/ContractViews/ContractViews.svelte';
 	import { page } from '$app/stores';
 	import { formatDate } from '$lib/helpers.js';
+	import Table from '$lib/components/app/Table/Table.svelte';
 
 	export let data;
 
@@ -103,72 +103,6 @@
 		isMounted = true;
 	});
 
-	const columns = [
-		{ type: 'field', field: 'solicitation.number' },
-		{ type: 'field', field: 'solicitation.description' },
-		{ type: 'field', field: 'solicitation.expires_on' },
-		{ type: 'formula', field: 'market_value' },
-		{ type: 'field', field: 'solicitation.estimated_value' },
-		{ type: 'status', status: 'opportunity' },
-		{ type: 'status', status: 'engineering' },
-		{ type: 'status', status: 'bom' },
-		{ type: 'status', status: 'purchasing' },
-		{ type: 'status', status: 'labor' },
-		{ type: 'status', status: 'review' },
-		{ type: 'status', status: 'bid' },
-		{ type: 'field', field: 'solicitation.nsn.id', header: 'NSN' },
-		{ type: 'matching_rule', status: 'matching_rule' },
-		{ type: 'field', field: 'solicitation.set_aside' },
-		{ type: 'field', field: 'solicitation.issued_on' },
-		{ type: 'field', field: 'solicitation.quantity' },
-		{ type: 'field', field: 'solicitation.quantity_units' },
-		{ type: 'field', field: 'solicitation.first_article' },
-		{
-			type: 'field',
-			field: 'solicitation.nsn.matching_nsns',
-			array_selector: 'part_number',
-			header: 'In-House PN'
-		},
-		{ type: 'field', field: 'solicitation.days_to_deliver' },
-		{ type: 'field', field: 'bom_url' },
-		{ type: 'field', field: 'price_per_unit' },
-		{ type: 'link', field: 'solicitation.solicitation_url' },
-		{ type: 'link', field: 'solicitation.tech_docs' }
-	];
-
-	function getStatusColor(status) {
-		if (!status) return '';
-		let color = '';
-
-		switch (tags[status.toString().split(':')[0]][status.toString().split(':')[1]].color) {
-			case 'green':
-				color = 'bg-green-400';
-				break;
-			case 'yellow':
-				color = 'bg-yellow-400';
-				break;
-			case 'red':
-				color = 'bg-red-400';
-				break;
-			case 'blue':
-				color = 'bg-blue-400';
-				break;
-			case 'gray':
-				color = 'bg-gray-500';
-				break;
-		}
-		return color;
-	}
-
-	function getStatusName(status) {
-		if (!status) return '';
-		return tags[status.toString().split(':')[0]][status.toString().split(':')[1]].name;
-	}
-
-	function navToSolicitation(id) {
-		window.location.href = `${window.location.origin}/solicitation/${id}`;
-	}
-
 	const views = {
 		'/contracts/bidding-funnel': 'Bidding Funnel',
 		'/contracts/recently-released': 'Recently Released',
@@ -189,53 +123,7 @@
 </div>
 
 {#if solicitations_matched}
-	<article
-		class="bg-white w-[100%] h-[95%] px-2 overflow-y-auto overflow-x-auto border-l-[0.2px] border-l-gainsboro"
-		style="direction: ltr;"
-	>
-		<table class="text-left w-[100%] border-separate border-spacing-0">
-			<thead class="h-[32px] sticky bg-white" style="inset-block-start: 0;">
-				{#each columns as column}
-					<th>{tableFieldMapper(undefined, column).header}</th>
-				{/each}
-			</thead>
-
-			<tbody>
-				{#each solicitations_matched as solicitation_matched (solicitation_matched.id)}
-					<tr
-						on:click={() => navToSolicitation(solicitation_matched.id)}
-						class="hover:bg-neutral-100"
-					>
-						{#each columns as column, i}
-							{#if column.type === 'status'}
-								<td>
-									<div
-										class="p-2 rounded-md inline-block {getStatusColor(
-											tableFieldMapper(solicitation_matched, column).value
-										) ?? ''}"
-									>
-										{getStatusName(tableFieldMapper(solicitation_matched, column).value) ?? ''}
-									</div>
-								</td>
-							{:else if column.type === 'matching_rule'}
-								<td>{tableFieldMapper(solicitation_matched, column).value ?? ''}</td>
-							{:else if column.type === 'link'}
-								<td>
-									<a
-										href={tableFieldMapper(solicitation_matched, column).value ?? ''}
-										target="_blank"
-										class="mb-5 text-blue-500">URL</a
-									>
-								</td>
-							{:else}
-								<td>{tableFieldMapper(solicitation_matched, column).value ?? ''}</td>
-							{/if}
-						{/each}
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</article>
+	<Table data={solicitations_matched} />
 {:else}
 	<div class="flex flex-col gap-4 p-5">
 		<div class="skeleton h-4 w-full"></div>
@@ -248,19 +136,3 @@
 		<div class="skeleton h-4 w-full"></div>
 	</div>
 {/if}
-
-<style>
-	th {
-		border-bottom: 1px solid #e0e0e0;
-		border-top: 1px solid #e0e0e0;
-		white-space: nowrap;
-		border: 0.2px solid gainsboro;
-		padding: 4px;
-	}
-
-	td {
-		border: 0.2px solid gainsboro;
-		white-space: nowrap;
-		padding: 4px;
-	}
-</style>
