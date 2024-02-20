f6b6b13882ef29be89cba8dbeaa6ae6564af73a6
NelsonRay <nelsonray27@gmail.com>
2024-02-19 20:19:54 -0500

    Make 50/50 width for solicitation form

--- src/lib/components/app/SolicitationForm/SolicitationForm.svelte
+++ src/lib/components/app/SolicitationForm/SolicitationForm.svelte
@@ -289,7 +289,7 @@
 		top: 0;
 		left: 0;
 		bottom: 0;
-		width: 60%;
+		width: 50%;
 		display: flex;
 		flex-direction: column;
 		/* background: #333; */
@@ -297,9 +297,9 @@
 	.two {
 		position: absolute;
 		top: 0;
-		left: 60%;
+		left: 50%;
 		bottom: 0;
-		width: 40%;
+		width: 50%;
 		overflow-y: auto;
 		/* background: #999; */
 		/* height: 120%; */
