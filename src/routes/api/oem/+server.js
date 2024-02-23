db642bc028aa0a3adcf36d9fab8dbc4f490832c0
NelsonRay <nelsonray27@gmail.com>
2024-02-22 23:44:07 -0600

    OEM form WIP

--- src/routes/api/oem/+server.js
+++ src/routes/api/oem/+server.js
@@ -1,5 +1,4 @@
 // @ts-nocheck
-import { AIRTABLE_API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 /** @type {import('@sveltejs/adapter-vercel').Config} */
@@ -8,47 +7,34 @@ export const config = {
 	split: true
 };
 
-export async function POST({ request, fetch }) {
+export async function POST({ request, locals: { supabase } }) {
 	const { value } = await request.json();
 
-	const url = 'https://api.airtable.com/v0/appckqmLpLvXCi7rA/tblNv96zolsGAmdM9';
+	const { customer, parts, ...rest } = value;
 
-	let Parts = '';
-	let Quantities = '';
+	let customerId = customer.id;
 
-	for (let part of value.parts) {
-		if (Parts !== '' && Quantities !== '') {
-			Parts += '\n';
-			Quantities += '\n';
-		}
+	if (!customerId) {
+		const { data: cData, error: cError } = await supabase
+			.from('oem_customers')
+			.upsert(customer)
+			.select('id')
+			.limit(1)
+			.single();
 
-		Parts += `${part.part_number},${part.nsn},${part.cross_ref}`;
-		Quantities += part.quantities.join(',');
+		customerId = cData.id;
 	}
 
-	let records = [
-		{
-			fields: {
-				'Company Name': value.customer_name,
-				'Customer Number': value.customer_number,
-				Date: value.date,
-				'Expiration Date': value.return_date,
-				'Customer Contact': value.customer_email,
-				"Cindy's Notes": value.notes,
-				Parts,
-				Quantities
-			}
-		}
-	];
+	const { data, error } = await supabase
+		.from('oem_rfqs')
+		.upsert({ ...rest, customer: customerId, firm: '6b289746-2b01-47af-a7d4-26a3920f75ca' })
+		.select('id')
+		.limit(1)
+		.single();
 
-	const res = await fetch(url, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${AIRTABLE_API_KEY}` },
-		body: JSON.stringify({ records })
-	});
+	const { data: pData, error: pError } = await supabase
+		.from('oem_rfqs_parts')
+		.upsert(parts.map((p) => ({ ...p, oem_rfq: data.id })));
 
-	const data = await res.json();
-	console.log(res.status, data);
-
-	return json(data, { status: res.status });
+	return json({}, { status: 200 });
 }
--- src/routes/oem-form/[slug]/+page.svelte
+++ src/routes/oem-form/[slug]/+page.svelte
@@ -1,16 +1,14 @@
 <script>
 	let value = {
-		customer_name: '',
-		customer_email: '',
-		customer_number: '',
-		date: '',
-		return_date: '',
+		customer: { name: '', email_address: '', customer_number: '' },
+		date_received: '',
+		requested_return_date: '',
 		parts: [
 			{
 				part_number: '',
 				nsn: '',
-				cross_ref: '',
-				quantities: ['']
+				cross_reference: '',
+				quantities: [null]
 			}
 		],
 		notes: ''
@@ -38,23 +36,16 @@
 		value.parts.push({
 			part_number: '',
 			nsn: '',
-			cross_ref: '',
-			quantities: ['']
+			cross_reference: '',
+			quantities: [null]
 		});
 
 		value = value;
 	}
 
-	function resetParts() {
-		value.parts = [
-			{
-				part_number: '',
-				nsn: '',
-				cross_ref: '',
-				quantities: ['']
-			}
-		];
-
+	// @ts-ignore
+	function removePart(index) {
+		value.parts.splice(index, 1);
 		value = value;
 	}
 
@@ -62,7 +53,7 @@
 	 * @param {number} index
 	 */
 	function addQty(index) {
-		value.parts[index].quantities.push('');
+		value.parts[index].quantities.push(null);
 
 		value = value;
 	}
@@ -71,13 +62,13 @@
 	 * @param {number} index
 	 */
 	function resetQuantities(index) {
-		value.parts[index].quantities = [''];
+		value.parts[index].quantities = [null];
 
 		value = value;
 	}
 </script>
 
-<div class="flex w-screen justify-center">
+<div class="flex h-screen w-screen justify-center overflow-y-auto">
 	<div class="flex flex-col">
 		{#if !submitted}
 			<h1 class="text-3xl mb-8 mt-8">New OEM RFQ Form</h1>
@@ -86,108 +77,136 @@
 					<label for="customer_name">Customer Name</label>
 					<input
 						id="customer_name"
+						autocomplete="off"
 						class="border border-blue-400"
-						bind:value={value.customer_name}
+						bind:value={value.customer.name}
 					/>
 				</div>
 				<div class="flex flex-col">
 					<label for="customer_email">Customer Email</label>
 					<input
 						id="customer_email"
+						autocomplete="off"
 						class="border border-blue-400"
-						bind:value={value.customer_email}
+						bind:value={value.customer.email_address}
 					/>
 				</div>
 				<div class="flex flex-col">
 					<label for="customer_number">Customer Number</label>
 					<input
 						id="customer_number"
+						autocomplete="off"
 						class="border border-blue-400"
-						bind:value={value.customer_number}
+						bind:value={value.customer.customer_number}
 					/>
 				</div>
 				<div class="flex flex-col">
 					<label for="date_received">Date Received</label>
 					<input
 						id="date_received"
+						autocomplete="off"
 						type="date"
 						class="border border-blue-400"
-						bind:value={value.date}
+						bind:value={value.date_received}
 					/>
 				</div>
 				<div class="flex flex-col">
 					<label for="return_date">Return Date</label>
 					<input
 						id="return_date"
+						autocomplete="off"
 						type="date"
 						class="border border-blue-400"
-						bind:value={value.return_date}
+						bind:value={value.requested_return_date}
 					/>
 				</div>
 			</div>
 			<p class="text-xl font-semibold mt-8 mb-4">Parts</p>
 			<div class="flex flex-col space-y-7">
 				{#each value.parts as part, index}
-					<div>
-						<p class="text-lg font-semibold mb-2">Part #{index + 1}</p>
-						<div class="grid grid-cols-3 gap-5">
-							<div class="flex flex-col">
-								<label for="part_number">Part Number</label>
-								<input
-									id="part_number"
-									class="border border-blue-400"
-									bind:value={part.part_number}
-								/>
+					<div class="flex flex-row items-center bg-neutral-100 rounded-md">
+						<p class="text-lg font-semibold mb-2 mx-3">#{index + 1}</p>
+						<div class="flex flex-col my-2">
+							<div class="flex flex-row space-x-3">
+								<div class="flex flex-col">
+									<label class="text-xs text-gray-500 font-medium" for="part_number"
+										>Part Number</label
+									>
+									<input
+										id="part_number"
+										autocomplete="off"
+										class="border border-blue-400"
+										bind:value={part.part_number}
+									/>
+								</div>
+								<div class="flex flex-col">
+									<label class="text-xs text-gray-500 font-medium" for="nsn">NSN</label>
+									<input
+										id="nsn"
+										autocomplete="off"
+										class="border border-blue-400"
+										bind:value={part.nsn}
+									/>
+								</div>
+								<div class="flex flex-col">
+									<label class="text-xs text-gray-500 font-medium" for="cross_ref">Cross Ref</label>
+									<input
+										id="cross_ref"
+										autocomplete="off"
+										class="border border-blue-400"
+										bind:value={part.cross_reference}
+									/>
+								</div>
 							</div>
-							<div class="flex flex-col">
-								<label for="nsn">NSN</label>
-								<input id="nsn" class="border border-blue-400" bind:value={part.nsn} />
-							</div>
-							<div class="flex flex-col">
-								<label for="cross_ref">Cross Ref</label>
-								<input id="cross_ref" class="border border-blue-400" bind:value={part.cross_ref} />
+							<div class="flex flex-col mt-2">
+								<p class="text-xs text-gray-500 font-medium">Quantities</p>
+								<div class="flex flex-row justify-between">
+									<div class="flex flex-row space-x-2">
+										{#each part.quantities as qty}
+											<input
+												autocomplete="off"
+												class="border border-blue-400 w-16"
+												type="number"
+												bind:value={qty}
+											/>
+										{/each}
+										<button class="bg-blue-50 mx-5 px-2 rounded-md" on:click={() => addQty(index)}
+											>+</button
+										>
+									</div>
+									{#if part.quantities.length > 1}
+										<button
+											class="bg-blue-50 px-2 text-sm rounded-md"
+											on:click={() => resetQuantities(index)}
+										>
+											Reset
+										</button>
+									{/if}
+								</div>
 							</div>
 						</div>
-						<div class="flex flex-col mt-5">
-							<p>Quantities</p>
-							<div class="flex flex-row justify-between">
-								<div class="flex flex-row space-x-2">
-									{#each part.quantities as qty}
-										<input class="border border-blue-400 w-16" bind:value={qty} />
-									{/each}
-									<button class="bg-blue-50 mx-5 px-2 rounded-md" on:click={() => addQty(index)}
-										>+</button
-									>
-								</div>
-								{#if part.quantities.length > 1}
-									<button
-										class="bg-blue-50 px-2 text-sm rounded-md"
-										on:click={() => resetQuantities(index)}
-									>
-										Reset
-									</button>
-								{/if}
-							</div>
+						<div class="w-10">
+							{#if index > 0}
+								<button class="w-10" on:click={() => removePart(index)}>
+									<p class="text-center text-gray-500">X</p>
+								</button>
+							{/if}
 						</div>
 					</div>
 				{/each}
 			</div>
-			<div class="flex flex-row justify-between mt-8 -mx-1">
-				<button on:click={addPart} class="bg-blue-50 px-2 rounded-md text-lg font-medium"
-					>Add Part</button
-				>
-				{#if value.parts.length > 1}
-					<button on:click={resetParts} class="bg-blue-50 px-2 rounded-md text-md"
-						>Reset Parts</button
-					>
-				{/if}
-			</div>
+
+			<button
+				on:click={addPart}
+				class="bg-neutral-50 p-1 mt-6 rounded-md text-base font-medium w-full">Add Part</button
+			>
+
 			<p class="text-lg mt-10 font-medium">Notes</p>
 			<textarea
 				class="flex min-h-16 overflow-y-auto border border-blue-400"
 				bind:value={value.notes}
 			/>
-			<p class="mt-10">Please ensure all necessary text fields are filled in before submitting!</p>
+
 			<div class="flex flex-row mt-2 text-lg font-medium">
 				<button class="bg-blue-50 px-2 rounded-md -ml-2" on:click={submit}>Submit</button>
 			</div>
