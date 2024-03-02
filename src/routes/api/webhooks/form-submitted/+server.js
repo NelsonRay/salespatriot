b4bc6454827d35b2adec3cf710c866648d9d8990
NelsonRay <nelsonray27@gmail.com>
2024-03-02 17:43:12 -0600

    Text error catching for forms webhook

--- src/routes/api/webhooks/form-submitted/+server.js
+++ src/routes/api/webhooks/form-submitted/+server.js
@@ -35,95 +35,101 @@ export async function POST({ request, cookies }) {
 		.single();
 
 	if (error) {
-		console.log(error);
+		console.error(error);
+		return json({ error }, { status: 500 });
 	}
 
-	switch (form) {
-		case '50e95568-180b-46d5-a341-f216bb2a3c17':
-			if (response.skip_engineering) {
-				await updateStatusInProgress(
-					data.status,
-					['purchasing:in_progress', 'labor:in_progress'],
-					supabase,
-					solicitation_matched
-				);
-				await supabase
-					.from('forms')
-					.insert({ form: '18055704-d9b9-42d7-958b-f5d1d5b1ba4d', solicitation_matched });
+	try {
+		switch (form) {
+			case '50e95568-180b-46d5-a341-f216bb2a3c17':
+				if (response.skip_engineering) {
+					await updateStatusInProgress(
+						data.status,
+						['purchasing:in_progress', 'labor:in_progress'],
+						supabase,
+						solicitation_matched
+					);
+					await supabase
+						.from('forms')
+						.insert({ form: '18055704-d9b9-42d7-958b-f5d1d5b1ba4d', solicitation_matched });
 
-				await supabase
-					.from('forms')
-					.insert({ form: '53cc6979-4406-47aa-97a0-1d83d0504c12', solicitation_matched });
-			} else if (data.status.includes('opportunity:pursue')) {
-				await updateStatusInProgress(
-					data.status,
-					['engineering:in_progress'],
-					supabase,
-					solicitation_matched
-				);
-				await supabase
-					.from('forms')
-					.insert({ form: '0ce91293-4277-446e-9ff2-2d5c62bbb0fe', solicitation_matched });
-			}
-			break;
-		case '0ce91293-4277-446e-9ff2-2d5c62bbb0fe':
-			if (data.status.includes('engineering:can_build')) {
-				await updateStatusInProgress(
-					data.status,
-					['labor:in_progress'],
-					supabase,
-					solicitation_matched
-				);
-				await supabase
-					.from('forms')
-					.insert({ form: 'bee07e8a-3c83-4bce-89a7-f91ca65804e6', solicitation_matched });
-			}
+					await supabase
+						.from('forms')
+						.insert({ form: '53cc6979-4406-47aa-97a0-1d83d0504c12', solicitation_matched });
+				} else if (data.status.includes('opportunity:pursue')) {
+					await updateStatusInProgress(
+						data.status,
+						['engineering:in_progress'],
+						supabase,
+						solicitation_matched
+					);
+					await supabase
+						.from('forms')
+						.insert({ form: '0ce91293-4277-446e-9ff2-2d5c62bbb0fe', solicitation_matched });
+				}
+				break;
+			case '0ce91293-4277-446e-9ff2-2d5c62bbb0fe':
+				if (data.status.includes('engineering:can_build')) {
+					await updateStatusInProgress(
+						data.status,
+						['labor:in_progress'],
+						supabase,
+						solicitation_matched
+					);
+					await supabase
+						.from('forms')
+						.insert({ form: 'bee07e8a-3c83-4bce-89a7-f91ca65804e6', solicitation_matched });
+				}
 
-			break;
-		case 'bee07e8a-3c83-4bce-89a7-f91ca65804e6':
-			if (data.status.includes('bom:created') || data.status.included('bom:in_house_part')) {
-				await updateStatusInProgress(
-					data.status,
-					['purchasing:in_progress', 'labor:in_progress'],
-					supabase,
-					solicitation_matched
-				);
-				await supabase
-					.from('forms')
-					.insert({ form: '18055704-d9b9-42d7-958b-f5d1d5b1ba4d', solicitation_matched });
+				break;
+			case 'bee07e8a-3c83-4bce-89a7-f91ca65804e6':
+				if (data.status.includes('bom:created') || data.status.included('bom:in_house_part')) {
+					await updateStatusInProgress(
+						data.status,
+						['purchasing:in_progress', 'labor:in_progress'],
+						supabase,
+						solicitation_matched
+					);
+					await supabase
+						.from('forms')
+						.insert({ form: '18055704-d9b9-42d7-958b-f5d1d5b1ba4d', solicitation_matched });
 
-				await supabase
-					.from('forms')
-					.insert({ form: '53cc6979-4406-47aa-97a0-1d83d0504c12', solicitation_matched });
-			}
-			break;
-		case '18055704-d9b9-42d7-958b-f5d1d5b1ba4d':
-		case '53cc6979-4406-47aa-97a0-1d83d0504c12':
-			if (
-				data.status.includes('purchasing:within_budget') ||
-				data.status.includes('labor:within_time')
-			) {
-				await updateStatusInProgress(
-					data.status,
-					['review:in_progress'],
-					supabase,
-					solicitation_matched
-				);
-				await supabase
-					.from('forms')
-					.insert({ form: '6bbf4342-1b50-4c1a-9dc5-ad40562c5626', solicitation_matched });
-			}
-			break;
+					await supabase
+						.from('forms')
+						.insert({ form: '53cc6979-4406-47aa-97a0-1d83d0504c12', solicitation_matched });
+				}
+				break;
+			case '18055704-d9b9-42d7-958b-f5d1d5b1ba4d':
+			case '53cc6979-4406-47aa-97a0-1d83d0504c12':
+				if (
+					data.status.includes('purchasing:within_budget') ||
+					data.status.includes('labor:within_time')
+				) {
+					await updateStatusInProgress(
+						data.status,
+						['review:in_progress'],
+						supabase,
+						solicitation_matched
+					);
+					await supabase
+						.from('forms')
+						.insert({ form: '6bbf4342-1b50-4c1a-9dc5-ad40562c5626', solicitation_matched });
+				}
+				break;
 
-		case '6bbf4342-1b50-4c1a-9dc5-ad40562c5626':
-			await supabase
-				.from('forms')
-				.insert({ form: '6a0d1585-d572-4d8f-bdb4-498a89506e85', solicitation_matched });
-			break;
-		case '6a0d1585-d572-4d8f-bdb4-498a89506e85':
-			break;
-		default:
-			break;
+			case '6bbf4342-1b50-4c1a-9dc5-ad40562c5626':
+				await supabase
+					.from('forms')
+					.insert({ form: '6a0d1585-d572-4d8f-bdb4-498a89506e85', solicitation_matched });
+				break;
+			case '6a0d1585-d572-4d8f-bdb4-498a89506e85':
+				break;
+			default:
+				break;
+		}
+	} catch (e) {
+		console.error(e);
+		return json({ e }, { status: 500 });
 	}
 
 	return json({}, { status: 200 });
